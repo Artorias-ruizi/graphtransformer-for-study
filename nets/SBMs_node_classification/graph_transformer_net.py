4691262f119d187eea45ebd8a79654512d677b26
5Ruiz <1195479078@qq.com>
2023-10-28 11:53:41 +0800

    update graph_transformer_net.py

--- nets/SBMs_node_classification/graph_transformer_net.py
+++ nets/SBMs_node_classification/graph_transformer_net.py
@@ -11,12 +11,13 @@ import dgl
 from layers.graph_transformer_layer import GraphTransformerLayer
 from layers.mlp_readout_layer import MLPReadout
 
+
 class GraphTransformerNet(nn.Module):
 
     def __init__(self, net_params):
-        super().__init__()
+        super(GraphTransformerNet, self).__init__()
 
-        in_dim_node = net_params['in_dim'] # node_dim (feat is an integer)
+        in_dim_node = net_params['in_dim']  # node_dim (feat is an integer)
         hidden_dim = net_params['hidden_dim']
         out_dim = net_params['out_dim']
         n_classes = net_params['n_classes']
@@ -34,46 +35,50 @@ class GraphTransformerNet(nn.Module):
         self.device = net_params['device']
         self.lap_pos_enc = net_params['lap_pos_enc']
         self.wl_pos_enc = net_params['wl_pos_enc']
-        max_wl_role_index = 100 
-        
-        if self.lap_pos_enc:
+        max_wl_role_index = 100
+
+        if self.lap_pos_enc:  # PE Embedding: Laplacian
             pos_enc_dim = net_params['pos_enc_dim']
             self.embedding_lap_pos_enc = nn.Linear(pos_enc_dim, hidden_dim)
-        if self.wl_pos_enc:
+        if self.wl_pos_enc:  # PE Embedding: WL
             self.embedding_wl_pos_enc = nn.Embedding(max_wl_role_index, hidden_dim)
-        
-        self.embedding_h = nn.Embedding(in_dim_node, hidden_dim) # node feat is an integer
-        
-        self.in_feat_dropout = nn.Dropout(in_feat_dropout)
-        
-        self.layers = nn.ModuleList([GraphTransformerLayer(hidden_dim, hidden_dim, num_heads,
-                                              dropout, self.layer_norm, self.batch_norm, self.residual) for _ in range(n_layers-1)])
-        self.layers.append(GraphTransformerLayer(hidden_dim, out_dim, num_heads, dropout, self.layer_norm, self.batch_norm,  self.residual))
-        self.MLP_layer = MLPReadout(out_dim, n_classes)
 
+        self.embedding_h = nn.Embedding(in_dim_node, hidden_dim)  # node feat is an integer. For input embedding
+
+        self.in_feat_dropout = nn.Dropout(in_feat_dropout)  # set dropout ratio and conduct dropout by a dropout layer
+
+        self.layers = nn.ModuleList([GraphTransformerLayer(hidden_dim, hidden_dim, num_heads,
+                                                           dropout, self.layer_norm, self.batch_norm, self.residual) for
+                                     _ in range(n_layers - 1)])  # set module list for all transformer layers. PS:
+        # Expect the last transformer layer. For their output dim is different.
+        self.layers.append(
+            GraphTransformerLayer(hidden_dim, out_dim, num_heads, dropout, self.layer_norm, self.batch_norm,
+                                  self.residual))  # the last transformer layer.
+        self.MLP_layer = MLPReadout(out_dim, n_classes)  # at last add an MLP as readout module.
 
     def forward(self, g, h, e, h_lap_pos_enc=None, h_wl_pos_enc=None):
 
         # input embedding
         h = self.embedding_h(h)
+        # conduct PE
         if self.lap_pos_enc:
-            h_lap_pos_enc = self.embedding_lap_pos_enc(h_lap_pos_enc.float()) 
+            h_lap_pos_enc = self.embedding_lap_pos_enc(h_lap_pos_enc.float())
             h = h + h_lap_pos_enc
         if self.wl_pos_enc:
-            h_wl_pos_enc = self.embedding_wl_pos_enc(h_wl_pos_enc) 
+            h_wl_pos_enc = self.embedding_wl_pos_enc(h_wl_pos_enc)
             h = h + h_wl_pos_enc
+        # conduct dropout
         h = self.in_feat_dropout(h)
-        
-        # GraphTransformer Layers
+
+        # Forward for GraphTransformer Layers
         for conv in self.layers:
             h = conv(g, h)
-            
-        # output
+
+        # output from an MLP readout module.
         h_out = self.MLP_layer(h)
 
         return h_out
-    
-    
+
     def loss(self, pred, label):
 
         # calculating label weights for weighted loss computation
@@ -83,14 +88,10 @@ class GraphTransformerNet(nn.Module):
         cluster_sizes = torch.zeros(self.n_classes).long().to(self.device)
         cluster_sizes[torch.unique(label)] = label_count
         weight = (V - cluster_sizes).float() / V
-        weight *= (cluster_sizes>0).float()
-        
+        weight *= (cluster_sizes > 0).float()
+
         # weighted cross-entropy for unbalanced classes
         criterion = nn.CrossEntropyLoss(weight=weight)
         loss = criterion(pred, label)
 
         return loss
-
-
-
-        
